00b263fc8c5b8c1999ee8890b95dfa7c3d816890
Farai Major <faraimutukumira@gmail.com>
2023-03-18 21:25:46 -0400

    set up props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
+// import Confetti from "react-confetti"
+import Die from "./components/Die"
 import './App.css';
 
 function App() {
+
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  const diceElements = dice.map(die => <Die value={die} />)
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="dice-container">
+        {diceElements}
+      </div>
+      <div className="btn">
+        <button onClick={rollDice}>Roll</button>
+      </div>
+
+    </main>
   );
 }
 
